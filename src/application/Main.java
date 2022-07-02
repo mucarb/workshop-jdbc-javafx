af5a42e42d453e7aec8393b3e5c11f7292e7fab3
mucarb <murilo.rb60@gmail.com>
2022-07-02 20:56:34 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,8 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+	private static Scene mainScene;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -18,7 +20,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
@@ -26,6 +28,10 @@ public class Main extends Application {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 
 	public static void main(String[] args) {
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
@@ -17,7 +26,7 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	private MenuItem menuItemAbout;
-	
+
 	@FXML
 	public void onMenuItemSellerAction() {
 		System.out.println("onMenuItemSellerAction");
@@ -30,11 +39,38 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/About.fxml");
 	}
-	
+
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
+	}
+
+	// synchronized garante que o processo não seja interrompido durante multthread
+	private synchronized void loadView(String absoluteName) {
+		try {
+			// carregando arquivo fxml
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			// ao carregar o arquivo, é atribuido em um layout VBox
+			VBox newVBox = loader.load();
+			// layout principal é atribuido em uma variável
+			Scene mainScene = Main.getMainScene();
+			// getRoot pega o primeiro elemento da view
+			// ScrollPane getContent acessando o conteudo referente do ScrollPane
+			// casting para tipo VBox
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			// pega os children do VBox na primeira posicao (main menu)
+			Node mainMenu = mainVBox.getChildren().get(0);
+
+			// limpa todos os childrens do main VBox
+			mainVBox.getChildren().clear();
+			// adicionando o main menu
+			mainVBox.getChildren().add(mainMenu);
+			// adicionando novo VBox a scene
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
